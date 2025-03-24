e189cdab63f7a356d98c44888870505c0eee85ef
Jaehyeop <hyeop3228@gmail.com>
2025-03-24 19:22:38 +0900

    Add files via upload

---
+++ Week_03/In_Class/Comment.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Comment(props){
+    return(
+        <div>
+            <h1>댓글 컴포넌트 만들기</h1>
+        </div>
+    )
+}
+
+export default Comment;
---
+++ Week_03/In_Class/CommentList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Comment from "./Comment.jsx";
+
+function CommentList(props){
+    return(
+        <div>
+            <h1><Comment /></h1>
+        </div>
+    )
+}
+export default Comment;
