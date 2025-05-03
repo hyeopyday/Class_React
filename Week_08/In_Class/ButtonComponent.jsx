1761ea47bf3830b029575b56d7634df1ccc04c99
Jaehyeop <hyeop3228@gmail.com>
2025-05-04 00:07:35 +0900

    Add files via upload

---
+++ Week_08/In_Class/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+import React, { camelCase } from "react";
+
+function ButtonComponent() {
+    function handleClick() {
+    alert("버튼이 클릭되었습니다!");
+    }
+    return <button onClick={handleClick}>클릭</button>;
+    }
+    
+    export default ButtonComponent;
